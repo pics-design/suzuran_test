a41ecfdc5279160e624ade51cc110df57aff9194
河野太平 <kounotahei@kounotaheinoMacBook-Pro.local>
2021-01-31 11:06:40 +0900

    Updates

--- precache-manifest.7e2eac4abb53a72fdfa87b1cb73ced4c.js
+++ precache-manifest.7e2eac4abb53a72fdfa87b1cb73ced4c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5699610f5562a2b8827d5bbd153ed975",
+    "revision": "ebae643636718fa9dcbdfbc101d8ab7e",
     "url": "/suzuran_test/index.html"
   },
   {
-    "revision": "152b1433c6727901c2f2",
+    "revision": "51c3c4f067a14be22a57",
     "url": "/suzuran_test/static/css/main.164c9c1c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/suzuran_test/static/js/2.8f79681d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "152b1433c6727901c2f2",
-    "url": "/suzuran_test/static/js/main.564bb037.chunk.js"
+    "revision": "51c3c4f067a14be22a57",
+    "url": "/suzuran_test/static/js/main.714b91fb.chunk.js"
   },
   {
     "revision": "2bcd341cc6bb94b38cef",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/suzuran_test/precache-manifest.f08645392b7de53a575f343849f8b6fd.js"
+  "/suzuran_test/precache-manifest.7e2eac4abb53a72fdfa87b1cb73ced4c.js"
 );
 
 self.addEventListener('message', (event) => {
