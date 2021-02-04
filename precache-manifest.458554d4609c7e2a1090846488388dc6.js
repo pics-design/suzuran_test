6e38bf4d2a96c6d937dd2594e21af34475a13712
河野太平 <kounotahei@kounotaheinoMacBook-Pro.local>
2021-02-04 20:55:09 +0900

    Updates

---
+++ precache-manifest.458554d4609c7e2a1090846488388dc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15a96e8bcad89545e0e7871f9a52dcdc",
+    "url": "/suzuran_test/index.html"
+  },
+  {
+    "revision": "56dad4abc71fb6d30220",
+    "url": "/suzuran_test/static/css/main.4a708750.chunk.css"
+  },
+  {
+    "revision": "8675fb294b369545449d",
+    "url": "/suzuran_test/static/js/2.7cd6652e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/suzuran_test/static/js/2.7cd6652e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56dad4abc71fb6d30220",
+    "url": "/suzuran_test/static/js/main.20f5ba59.chunk.js"
+  },
+  {
+    "revision": "2bcd341cc6bb94b38cef",
+    "url": "/suzuran_test/static/js/runtime-main.c6b4bbb6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/suzuran_test/precache-manifest.336bbf7a5134b512b548b49ef78db1d1.js"
+  "/suzuran_test/precache-manifest.458554d4609c7e2a1090846488388dc6.js"
 );
 
 self.addEventListener('message', (event) => {
